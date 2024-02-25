7ac8274ad4e15f87b024b05eb21ad3230eb2d250
manlly07 <dvc.290603@gmail.com>
2024-02-25 19:57:33 +0700

    manlly courses page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import Login from './pages/auth/Login'
 import Logout from './pages/auth/Logout'
 import Home from './pages/Home'
+import Courses from './pages/Courses'
 
 function App() {
 
   return (
     // <Login />
     // <Logout />
-    <Home />
+    // <Home />
+    <Courses />
+
   )
 }
 
---
+++ client/src/pages/CourseDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../layout/Layout'
+
+const CourseDetail = () => {
+  return (
+    <Layout>
+        
+    </Layout>
+  )
+}
+
+export default CourseDetail

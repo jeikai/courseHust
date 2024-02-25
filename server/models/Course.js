73724ea425b25e32a36b0b759191a4bfacc4f4dd
ducnm71 <21021293@vnu.edu.vn>
2024-02-25 23:04:39 +0700

    init

---
+++ server/models/Course.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const courseSchema = new Schema({
+    instuctorId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    level: { type: String, required: true },
+    language: { type: String, required: true },
+    tags: [{ type: String, required: true }],
+    price: { type: Number, required: true },
+    thumbnail: { type: String, required: true },
+    date_created: Date,
+    date_updated: Date
+})
+
+const Course = mongoose.model('Course', courseSchema, 'courses')
+exports.schema = Course

a4cb532082eebe245885c4bbb7fbe2c216dda07c
jeikai <98737616+jeikai@users.noreply.github.com>
2024-03-28 09:46:58 +0700

    quiz

--- server/models/Quiz.js
+++ server/models/Quiz.js
@@ -1,1 +1,24 @@
- 
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const QuizSchema = new Schema({
+    title: { type: String, required: true },
+    ques: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'Question'
+    }],
+    date_created: Date,
+    date_updated: Date
+})
+
+const Quiz = mongoose.model('Quiz', QuizSchema, 'quizs')
+exports.schema = Quiz
+
+exports.create = async function (data) {
+    try {
+        const quizData = {
+            title: data.title
+        }
+    } catch (error) {
+        return { error: error }
+    }
+}
--- server/models/Section.js
+++ server/models/Section.js
@@ -5,15 +5,15 @@ const courseModel = require('./Course')
 const SectionSchema = new Schema({
     title: { type: String, required: true },
     spec: [
-            {
-                _id: {type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' }, 
-                type: { type: String, enum: ['lesson', 'quiz'], default: 'lesson'}
-            }, 
-            { 
-                _id: {type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' },
-                type: { type: String, enum: ['lesson', 'quiz'], default: 'quiz'}
-            }
-            ],
+        {
+            _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' },
+            type: { type: String, enum: ['lesson', 'quiz'], default: 'lesson' }
+        },
+        {
+            _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' },
+            type: { type: String, enum: ['lesson', 'quiz'], default: 'quiz' }
+        }
+    ],
     date_created: Date,
     date_updated: Date
 })
@@ -21,13 +21,13 @@ const SectionSchema = new Schema({
 const Section = mongoose.model('Section', SectionSchema, 'sections')
 exports.schema = Section
 
-exports.create = async function(data){
-    try{
-        const course = await courseModel.get({courseId: data.courseId})
+exports.create = async function (data) {
+    try {
+        const course = await courseModel.get({ courseId: data.courseId })
         const sections = course.sections
         const checkSection = sections.some(item => item.title === data.title)
-        if(checkSection) {
-            return {error: "section existed in course"}
+        if (checkSection) {
+            return { error: "section existed in course" }
         }
         const sectionData = {
             title: data.title,
@@ -39,31 +39,31 @@ exports.create = async function(data){
         await newSection.save()
         await courseModel.addSection(data.courseId, newSection._id)
         return newSection
-    }catch(err){
-        return {error: err}
+    } catch (err) {
+        return { error: err }
     }
 }
 
-exports.addSpec = async function(sectionId ,id, type){
-    try{
+exports.addSpec = async function (sectionId, id, type) {
+    try {
         const section = await Section.findById(sectionId)
-        if(!section) return {error:'section not found'}
+        if (!section) return { error: 'section not found' }
 
-        section.spec.push({_id: id, type: type})
+        section.spec.push({ _id: id, type: type })
         section.date_updated = new Date()
         section.markModified("spec")
         section.markModified("date_updated")
         await section.save()
-    }catch(err){
-        return {error: err}
+    } catch (err) {
+        return { error: err }
     }
 }
 
-exports.get = async function(data){
-    try{
+exports.get = async function (data) {
+    try {
         return await Section.findById(data.sectionId).populate('spec._id').where('spec.type').equals(data.specType)
-    }catch(err){
-        return {error: err}
+    } catch (err) {
+        return { error: err }
     }
 }
 

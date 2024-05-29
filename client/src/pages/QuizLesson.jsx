1224d1c35a8b8a52ea69a5f8dd48e758cad13407
jeikai <98737616+jeikai@users.noreply.github.com>
2024-05-30 00:56:31 +0700

    fix UI quiz

--- client/src/pages/QuizLesson.jsx
+++ client/src/pages/QuizLesson.jsx
@@ -31,7 +31,9 @@ const QuizLesson = () => {
   // let toggle = false;
 
   const [lesson, setLesson] = useState(null);
-  const [start, setStart] = useState(localStorage.getItem(`${id}_time`) || null);
+  const [start, setStart] = useState(
+    localStorage.getItem(`${id}_time`) || null
+  );
   const handleStartQuiz = () => {
     console.log("Start quiz");
     // let duaration = lesson.duaration
@@ -63,52 +65,14 @@ const QuizLesson = () => {
   return (
     <div className="py-16 px-4">
       <Row gutter={24}>
-        <Col span={8} push={16} className="py-2 shadow-lg h-fit">
-          <Typography.Title level={4} className="text-center">
-            Quiz
-          </Typography.Title>
-          <Collapse ghost expandIconPosition={"end"}>
-            <Collapse.Panel
-              header={
-                <Typography.Title
-                  className="w-full hover:text-[#754FFE]"
-                  level={5}
-                >
-                  {lesson?.title} 
-                </Typography.Title>
-              }
-              key="1"
-            >
-              <Space direction="vertical" className="w-full">
-                <Flex align="center" gap={12} className="px-3 py-2 rounded-md">
-                  <Checkbox />
-                  <Link className="flex-1 group">
-                    <Flex justify="space-between">
-                      <Flex vertical>
-                        <p className="font-semibold text-base group-hover:text-[#754FFE]">
-                          {responseAPI?.data?.content}
-                        </p>
-                        <span className="text-[#6c757d]">Jan-04-2024</span>
-                      </Flex>
-                      <p className="text-[#6c757d] text-base font-medium">
-                        {responseAPI?.data?.duration}
-                      </p>
-                    </Flex>
-                  </Link>
-                </Flex>
-                <Divider type="horizontal" />
-              </Space>
-            </Collapse.Panel>
-          </Collapse>
-        </Col>
-        <Col span={16} pull={8}>
+        <Col span={24} pull={0}>
           <div className="mb-4 mt-2">
             {/* <TimePicker onChange={(value) => console.log(value.format('hh-mm-ss'))} /> */}
             {/* <Video video={responseAPI.data.videoURL}/> */}
             {!start ? (
               <Quiz handleStartQuiz={handleStartQuiz} />
             ) : (
-              lesson && <Questions lesson={lesson} quizId={id}/>
+              lesson && <Questions lesson={lesson} quizId={id} />
             )}
           </div>
         </Col>

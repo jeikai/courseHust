07342b01799d7f4097ea035525fcc41d5c79f8c9
jeikai <98737616+jeikai@users.noreply.github.com>
2024-06-24 17:49:44 +0700

    nearly done

--- client/src/pages/admin/AddQuestion.jsx
+++ client/src/pages/admin/AddQuestion.jsx
@@ -37,6 +37,7 @@ import { createQuestions } from "../../api/quiz";
 import { useAPI } from "../../hooks/api";
 import { ViewContext } from "../../context/View";
 import Loader from "../../components/Loader";
+import { en } from "@faker-js/faker";
 
 const AddQuestion = () => {
   const breadcrumb = [
@@ -79,9 +80,9 @@ const AddQuestion = () => {
       enableMultipleChoice[index] = false;
     }
     console.log(enableMultipleChoice[index]);
-    questions[indexQuestion].options = questions[indexQuestion].options.map(
+    questions[index].options = questions[index].options.map(
       (option, i) => {
-        if (!enableMultipleChoice[index]) {
+        if (enableMultipleChoice[index] === false) {
           option.isSelected = indexOption === i;
         } else {
           if (i === indexOption && option.isSelected) {
@@ -339,19 +340,46 @@ const AddQuestion = () => {
                                     </Form.Item>
                                     <Button
                                       onClick={() => {
-                                        const deletedIsSelectedType = [
-                                          ...isSelectedType,
-                                        ];
-                                        deletedIsSelectedType[index] = false;
-                                        setIsSelectedType(
-                                          deletedIsSelectedType
-                                        );
-                                        console.log(
-                                          "aaaaa",
-                                          isSelectedType[index]
-                                        );
-                                        remove(field.name);
-                                      }}
+                                        const deletedIsSelectedType = [...isSelectedType];
+                                        const deletedEnableMultiple = [...enableAddAnswers];
+                                        const deletedEnableAddAnswers = [...enableAddAnswers];
+                                        console.log("deletedIsSelectedType", deletedIsSelectedType, deletedEnableAddAnswers, deletedEnableMultiple)
+                                        if (enableAddAnswers.length === 0 && index === enableAddAnswers.length -1) {
+                                          deletedIsSelectedType[index] = false;
+                                          deletedEnableMultiple[index] = false;
+                                          deletedEnableAddAnswers[index] = true
+                                        } else {
+                                          console.log("aaaaaaa", index)
+                                          for (let i = index; i < enableAddAnswers.length-1; i++) {
+                                            if (deletedIsSelectedType[i+1] !== undefined) {
+                                              deletedIsSelectedType[i] = deletedIsSelectedType[i+1]
+                                            } else {
+                                              deletedIsSelectedType[i] = false;
+                                            }
+
+                                            if (deletedEnableMultiple[i+1] !== undefined) {
+                                              deletedEnableMultiple[i] = deletedEnableMultiple[i+1];
+                                            } else {
+                                              deletedEnableMultiple[i] = false;
+                                            }
+
+                                            if (deletedEnableAddAnswers[i+1] !== undefined) {
+                                              deletedEnableAddAnswers[i] = deletedEnableAddAnswers[i+1]
+                                            } else {
+                                              deletedEnableAddAnswers[i] = true;
+                                            }
+                                          }
+                                          deletedIsSelectedType[enableAddAnswers.length-1] = false;
+                                          deletedEnableMultiple[enableAddAnswers.length-1] = false;
+                                          deletedEnableAddAnswers[enableAddAnswers.length-1] = true
+                                        }
+                                        // deletedIsSelectedType[index] = false;
+                                        setEnableAddAnswers(deletedEnableAddAnswers);
+                                        setEnableMultipleChoice(deletedEnableAddAnswers);
+                                        setIsSelectedType(deletedIsSelectedType)
+                                        console.log("after", isSelectedType, enableAddAnswers, enableMultipleChoice)
+                                        remove(field.name)}
+                                      }
                                       danger
                                       icon={<DeleteOutlined />}
                                     ></Button>

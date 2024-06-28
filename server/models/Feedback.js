88de45b04c5e69acaa94e8cd440b5b771673c6ee
jeikai <98737616+jeikai@users.noreply.github.com>
2024-06-28 07:17:43 +0700

    done changing to content based filtering algorithm

--- server/models/Feedback.js
+++ server/models/Feedback.js
@@ -55,3 +55,12 @@ exports.get = async function () {
         return { error: err };
     }
 }
+
+exports.getByUserId = async function (userId) {
+    try {
+        const result = await Feedback.find({ userId }).populate('userId');
+        return result;
+    } catch (err) {
+        return { error: err };
+    }
+}
